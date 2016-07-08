64c2c264cec8ced667c02f5993bf80fe0151d8db
Guoxudonggit <1554110779@qq.com>
2016-07-08 23:30:22 +0800

    Update TestGit.java

--- src/com/atguigu/git/TestGit.java
+++ src/com/atguigu/git/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("day day happy");
+		System.out.println("month");
 	}
 
 }
